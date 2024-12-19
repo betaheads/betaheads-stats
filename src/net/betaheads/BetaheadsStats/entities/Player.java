8673337b816fc896519fcd0888e135828d85ab13
volkovMikhail <volkov.work1@gmail.com>
2024-12-20 00:06:26 +0300

    update player entity

--- src/net/betaheads/BetaheadsStats/entities/Player.java
+++ src/net/betaheads/BetaheadsStats/entities/Player.java
@@ -1,12 +1,45 @@
 package net.betaheads.BetaheadsStats.entities;
 
+import net.betaheads.utils.Database;
+
 public class Player {
-  //db fields
-  public String username;
-  public long played_ms;
+  // database fields
+  private long id;
+  private String username;
+  private long played_ms;
 
-  //
+  // operational private fields
+  private long joinTimeMs;
 
+  public long getId() {
+    return this.id;
+  }
 
+  public String getUsername() {
+    return this.username;
+  }
 
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public void setJoinTimeMs(long joinTimeMs) {
+    this.joinTimeMs = joinTimeMs;
+  }
+
+  public long getTotalPlayedTime() {
+    return played_ms + getCurrentSessionPlayTime();
+  }
+
+  public long getCurrentSessionPlayTime() {
+    return System.currentTimeMillis() - (System.currentTimeMillis() - joinTimeMs);
+  }
+
+  public void saveDataToDb() {
+    Database.executeQuery(
+        "INSERT INTO players (username, played_ms) " +
+            "VALUES ('" + this.username + "'," + this.played_ms + ") " +
+            "ON DUPLICATE KEY UPDATE played_ms = " + this.played_ms + ";",
+        false, 0);
+  }
 }
